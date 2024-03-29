eb4bd0c9b537c6e348db0e7cd661fbfb2684892f
Admin <maayush235@gmail.com>
2024-03-29 23:38:54 +0530

    Radhey Radhey

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poster_app/views/splash_Screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,11 @@ class MyApp extends StatelessWidget {
 
         useMaterial3: true,
       ),
-      // home: ,
+     initialRoute: '/',
+
+      routes: {
+          '/' : (context) => Splash_Screen(),
+      },
     );
   }
 }
---
+++ lib/views/splash_Screen.dart
@@ -0,0 +1,34 @@
+
+
+import 'package:flutter/material.dart';
+
+class Splash_Screen extends StatefulWidget {
+  const Splash_Screen({super.key});
+
+  @override
+  State<Splash_Screen> createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  @override
+  Widget build(BuildContext context) {
+
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+     body: Center(
+       child: Container(
+         height: height*0.8,
+         width: width,
+         decoration: BoxDecoration(
+           image: DecorationImage(
+             image: AssetImage('assets/gifs/splash.gif'),
+             fit: BoxFit.contain
+           )
+         ),
+       ),
+     ),
+    );
+  }
+}
