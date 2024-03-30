c0769635529799414cd2d62ee5c50c2ac2216800
Admin <maayush235@gmail.com>
2024-03-30 12:02:25 +0530

    Radhey Radhey

--- lib/utils/Festivals.dart
+++ lib/utils/Festivals.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
 
 List festivalList=[
-  {
-    'Festival name' : 'Diwali',
-    'Text': 'Create your Diwali Templates',
-    'Images' : 'asset/img/festival_img/diwali.webp',
-    'Color' : Colors.redAccent,
-  },
-  {
-    'Festival name' : 'Raksha Bandhan',
-    'Text': 'Create your Raksha Bandhan Templates',
-    'Images' : 'asset/img/festival_img/raksha_bandhan.jpg',
-    'Color' : Colors.yellow,
-  },
-  {
-    'Festival name' : 'Pongal',
-    'Text': 'Create your Pongal Templates',
-    'Images' : 'asset/img/festival_img/pongal.jpg',
-    'Color' : Colors.purple,
-  },
+
   {
     'Festival name' : 'Makar Sakranti',
     'Text': 'Create your Makar Sakranti Templates',
@@ -26,10 +9,10 @@ List festivalList=[
     'Color' :Colors.red,
   },
   {
-    'Festival name' : 'Basant Panchami',
-    'Text': 'Create your Basant Panchami Templates',
-    'Images' : 'asset/img/festival_img/basant_panchami.jpg',
-    'Color' : Colors.blue,
+    'Festival name' : 'Karwa Chauth',
+    'Text': 'Create your Karwa Chauth Templates',
+    'Images' : 'asset/img/festival_img/Karwa_Chauth.jpg',
+    'Color' : Colors.teal,
   },
   {
     'Festival name' : 'Maha Shivratri',
@@ -38,70 +21,123 @@ List festivalList=[
     'Color' : Colors.brown,
   },
 
+
+  {
+    'Festival name' : 'Diwali',
+    'Text': 'Create your Diwali Templates',
+    'Images' : 'asset/img/festival_img/diwali.webp',
+    'Color' : Colors.redAccent,
+  },
+
+
+  {
+    'Festival name' : 'Raksha Bandhan',
+    'Text': 'Create your Raksha Bandhan Templates',
+    'Images' : 'asset/img/festival_img/raksha_bandhan.jpg',
+    'Color' : Colors.yellow,
+  },
+
+
+  {
+    'Festival name' : 'Pongal',
+    'Text': 'Create your Pongal Templates',
+    'Images' : 'asset/img/festival_img/pongal.jpg',
+    'Color' : Colors.purple,
+  },
+
+
+
+
+
+  {
+    'Festival name' : 'Basant Panchami',
+    'Text': 'Create your Basant Panchami Templates',
+    'Images' : 'asset/img/festival_img/basant_panchami.jpg',
+    'Color' : Colors.blue,
+  },
+
+
+
+
   {
     'Festival name' : 'Navratri',
     'Text': 'Create your Navratri Templates',
     'Images' : 'asset/img/festival_img/navratri.jpg',
     'Color' : Colors.red,
   },
+
+
   {
     'Festival name' : 'Holi',
     'Text': 'Create your holi Templates',
     'Images' : 'asset/img/festival_img/holi.jpg',
     'Color' : Colors.lightGreen,
   },
+
+
   {
     'Festival name' : 'Bhai dhuj',
     'Text': 'Create your Bhai dhuj Templates',
     'Images' : 'asset/img/festival_img/Bhai_dhuj.jpg',
     'Color' : Colors.pink,
   },
+
+
   {
     'Festival name' : 'Ganesh Chaturthi',
     'Text': 'Create your Ganesh Chaturthi Templates',
     'Images' : 'asset/img/festival_img/ganesh.jpg',
     'Color' : Colors.deepOrange,
   },
+
+
   {
     'Festival name' : 'Independence Day',
     'Text': 'Create your Independence Day Templates',
     'Images' : 'asset/img/festival_img/day.jpg',
     'Color' : Colors.orangeAccent,
   },
+
+
   {
     'Festival name' : 'Republic Day',
     'Text': 'Create your Republic Day Templates',
     'Images' : 'asset/img/festival_img/Republic_Day.jpg',
     'Color' : Colors.black54,
   },
+
+
   {
     'Festival name' : 'Krishna Janmashtami',
     'Text': 'Create your Krishna Janmashtami Templates',
     'Images' : 'asset/img/festival_img/Janmashtami.jpg',
     'Color' : Colors.green,
   },
+
+
   {
     'Festival name' : 'Dussera',
     'Text': 'Create your Dussera Templates',
     'Images' : 'asset/img/festival_img/Dussera.jpg',
     'Color' : Colors.red,
   },
+
+
   {
     'Festival name' : 'Ram Navami',
     'Text': 'Create your Ram Navami Templates',
     'Images' : 'asset/img/festival_img/Ram_Navami.jpg',
     'Color' : Colors.lightGreenAccent,
   },
+
+
   {
     'Festival name' : 'Saraswati Pooj',
     'Text': 'Create your Saraswati pooja Templates',
     'Images' : 'asset/img/festival_img/Saraswati_Pooj.jpg',
     'Color' :Colors.lightBlueAccent,
   },
-  {
-    'Festival name' : 'Karwa Chauth',
-    'Text': 'Create your Karwa Chauth Templates',
-    'Images' : 'asset/img/festival_img/Karwa_Chauth.jpg',
-    'Color' : Colors.teal,
-  },
+
+
+
 ];
