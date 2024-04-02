280b092a4d4c51e03d171b4ac173eb32fa100225
Admin <maayush235@gmail.com>
2024-04-02 10:12:25 +0530

    Radhey Radhey

--- lib/views/Edit_Post/Edit_Post_Screen.dart
+++ lib/views/Edit_Post/Edit_Post_Screen.dart
@@ -17,135 +17,133 @@ class _Edit_POst_ScreenState extends State<Edit_POst_Screen> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
 
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          // automaticallyImplyLeading: ,
-          foregroundColor: Colors.white ,
-          backgroundColor:  const Color(0xff1d243b),
+    return Scaffold(
+      appBar: AppBar(
+        // automaticallyImplyLeading: ,
+        foregroundColor: Colors.white ,
+        backgroundColor:  const Color(0xff1d243b),
 
-          actions: [
+        actions: [
 
-            IconButton(onPressed: () {
+          IconButton(onPressed: () {
 
-            }, icon: Icon(Icons.download))
+          }, icon: Icon(Icons.download))
 
-          ],
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  Container(
-                    height: height * 0.2,
-                    alignment: Alignment.center,
-                    decoration: const BoxDecoration(
-                        color: Color(0xff1d243b),
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(200),
-                            bottomRight: Radius.circular(200))),
-                  ),
-                  Container(
-                      margin: EdgeInsets.symmetric(
-                          horizontal: width * 0.1, vertical: height * 0.05),
-                      height: height * 0.35,
-                      width: width * 0.8,
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 0.5,
-                            spreadRadius: 1,
-                          )
-                        ],
-                        color: Colors.black,
-                      ),
-                      child: Image.asset(
-                        festivalList[postviewIndex]['Images'],
-                        fit: BoxFit.cover,
-                      )),
-
-                  // Edit container
-                  Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.fromLTRB(width*0.05, height*0.45, width*0.05, height*0),
-                    height: height*0.09,
-                    width: width*0.9,
-                    decoration: BoxDecoration(
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Container(
+                  height: height * 0.2,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
                       color: Color(0xff1d243b),
-                      borderRadius: BorderRadius.circular(15),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(200),
+                          bottomRight: Radius.circular(200))),
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(
+                        horizontal: width * 0.1, vertical: height * 0.05),
+                    height: height * 0.35,
+                    width: width * 0.8,
+                    decoration: BoxDecoration(
                       boxShadow: [
                         BoxShadow(
                           color: Colors.grey,
-                          blurRadius: 8,
-                          spreadRadius: 0.5,
-                          offset: Offset(1, 8)
-
+                          blurRadius: 0.5,
+                          spreadRadius: 1,
                         )
-                      ]
+                      ],
+                      color: Colors.black,
                     ),
-                    child: Text('Edit Template',style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.w800
+                    child: Image.asset(
+                      festivalList[postviewIndex]['Images'],
+                      fit: BoxFit.cover,
+                    )),
 
-                    ),),
+                // Edit container
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(width*0.05, height*0.45, width*0.05, height*0),
+                  height: height*0.09,
+                  width: width*0.9,
+                  decoration: BoxDecoration(
+                    color: Color(0xff1d243b),
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 8,
+                        spreadRadius: 0.5,
+                        offset: Offset(1, 8)
+
+                      )
+                    ]
+                  ),
+                  child: Text('Edit Template',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800
+
+                  ),),
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: height*0.262,
+            width: width,
+            alignment: Alignment.bottomRight,
+            decoration: const BoxDecoration(
+                color: Color(0xfffcbd05),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(220),
+                    topRight: Radius.circular(220))),
+            child: Container(
+              height: height*0.22,
+              width: width,
+
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                  color: Color(0xff1d243b),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(250),
+                      topRight: Radius.circular(250))),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  Text(
+                    'FestiVals',
+                    style: GoogleFonts.greatVibes(
+                      fontSize: 55,
+                      color: Colors.white,
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.bold, // Add this line for font weight
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'Create Your Template With us!!',
+                    style: GoogleFonts.greatVibes(
+                      fontSize: 22,
+                      color: Colors.white,
+                      letterSpacing: 2,
+                      fontWeight : FontWeight.bold
+                    ),
                   )
                 ],
               ),
             ),
-            Container(
-              height: height*0.262,
-              width: width,
-              alignment: Alignment.bottomRight,
-              decoration: const BoxDecoration(
-                  color: Color(0xfffcbd05),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(220),
-                      topRight: Radius.circular(220))),
-              child: Container(
-                height: height*0.22,
-                width: width,
-
-                alignment: Alignment.center,
-                decoration: const BoxDecoration(
-                    color: Color(0xff1d243b),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(250),
-                        topRight: Radius.circular(250))),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 35,
-                    ),
-                    Text(
-                      'FestiVals',
-                      style: GoogleFonts.greatVibes(
-                        fontSize: 55,
-                        color: Colors.white,
-                        letterSpacing: 2,
-                        fontWeight: FontWeight.bold, // Add this line for font weight
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      'Create Your Template With us!!',
-                      style: GoogleFonts.greatVibes(
-                        fontSize: 22,
-                        color: Colors.white,
-                        letterSpacing: 2,
-                        fontWeight : FontWeight.bold
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
--- lib/views/Home/Home_Screen.dart
+++ lib/views/Home/Home_Screen.dart
@@ -17,64 +17,66 @@ class _Home_ScreenState extends State<Home_Screen> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     // double width = MediaQuery.of(context).size.width;
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(65.0),
-          child: AppBar(
-            shadowColor: const Color(0xff1c2438),
-            centerTitle: true,
-            title: Text(
-              'Festy',
-              style: GoogleFonts.lobster(
-                fontSize: 35,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(65.0),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          shadowColor: const Color(0xff1c2438),
+          centerTitle: true,
+          toolbarHeight: 80,
+          title: Text(
+            'Festy',
+            style: GoogleFonts.greatVibes(
+              fontSize: 50,
+              fontWeight : FontWeight.w900,
+              color: Colors.white,
+              letterSpacing: 3,
+            ),
+          ),
+          backgroundColor: const Color(0xff1c2438),
+          actions: [
+            InkWell(
+              onTap: () {
+                setState(() {
+                  isfestival = !isfestival;
+                });
+              },
+              child: Icon((!isfestival)?Icons.list_outlined:
+              Icons.grid_view_rounded,
                 color: Colors.white,
-                letterSpacing: 2,
+                size: 30,
               ),
             ),
-            backgroundColor: const Color(0xff1c2438),
-            actions: [
-              InkWell(
-                onTap: () {
-                  setState(() {
-                    isfestival = !isfestival;
-                  });
-                },
-                child: Icon((!isfestival)?Icons.list_outlined:
-                Icons.grid_view,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(
-                width: 15,
-              )
-            ],
-          ),
+            SizedBox(
+              width: 15,
+            )
+          ],
         ),
-        body: Center(
-            child: Stack(
-              children: [
-                Column(
-                        children: [
-      
-                Container(
-                  height: height*0.22,
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                      color: Color(0xff1d243b),
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.elliptical(200, 200),
-                          bottomRight: Radius.elliptical(200, 200))),
-                ),
-      
-      
-                        ],
-                      ),
-                (isfestival)?List_view():Grid_View() ,
-              ],
-            )),
       ),
+      body: Center(
+          child: Stack(
+            children: [
+              Column(
+                      children: [
+
+              Container(
+                height: height*0.22,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                    color: Color(0xff1d243b),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.elliptical(200, 200),
+                        bottomRight: Radius.elliptical(200, 200))),
+              ),
+
+
+                      ],
+                    ),
+              (isfestival)?List_view():Grid_View() ,
+            ],
+          )),
     );
   }
 }
