8eaaf0a4b45d221b6faa71f70ae5041e10725fbe
Admin <maayush235@gmail.com>
2024-04-10 11:53:29 +0530

    Radhey Radhey

--- lib/utils/Text_Decoration.dart
+++ lib/utils/Text_Decoration.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-double top =0;
-double bottem =0;
-double left =0;
-double right =0;
-int alignmentindex =0;
+double top = 0;
+double bottem = 0;
+double left = 0;
+double right = 0;
+int alignmentindex = 0;
 List TextEditingControllerlist=[];
 TextEditingController txtname = TextEditingController(text: ' ');
-double fontofsize =15;
-List Colorlist =[
+double fontofsize = 15;
+List Colorlist = [
   Colors.white,
   Colors.red,
   Colors.blue,
@@ -23,7 +23,7 @@ List Colorlist =[
   Colors.deepPurple,
   Colors.pinkAccent
 ];
-int textcolorindex=0;
+int textcolorindex = 0;
 List textfamily = [
   GoogleFonts.nunito,
   GoogleFonts.playfairDisplay,
